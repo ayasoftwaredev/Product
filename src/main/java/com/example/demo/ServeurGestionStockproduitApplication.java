b8197839ef4fd14391a7d1d83a78673d17d8ea49
aymane <aymane@gmail.com>
2019-08-31 11:56:04 +0000

    Second_commit

---
+++ src/main/java/com/example/demo/ServeurGestionStockproduitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockproduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockproduitApplication.class, args);
+	}
+
+}
